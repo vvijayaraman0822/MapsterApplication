bab06e415d95d44d00de24a9923539e5250e7851
ChrisDuhan <chris.m.duhan@gmail.com>
2018-11-07 20:45:03 -0600

    Interfaces commented

--- src/models/messages/message.interface.ts
+++ src/models/messages/message.interface.ts
@@ -1,7 +1,7 @@
 export interface Message {
-    fromID: string;
-    toID: string;
-    message: string;
-    read: boolean;
-    timestamp: string;
+    fromID: string; // reference to the user id the message was sent from
+    toID: string; // reference to the user id the message is intended for
+    message: string; // the message itself
+    read: boolean; // has the message been read or not (or at least opened, we know how that goes)
+    timestamp: string; // timestamp of when the message was sent
 }
--- src/models/users/user.interface.ts
+++ src/models/users/user.interface.ts
@@ -1,12 +1,12 @@
 export interface User {
-    firstName: string;
-    lastName: string;
-    registerDate: string;
-    lastLogin: string;
-    email: string;
+    firstName: string; // First name
+    lastName: string; // Last name
+    registerDate: string; // timestamp of the moment the user first registered
+    lastLogin: string; // timestamp of the moment the last authentication happened
+    email: string; // email, duh
     // validated: boolean; Defined in firebase.User
-    profileImage: string;
-    publicImages: string;
-    privateImages: string;
-    friendsList: string[];
+    profileImage: string; // reference to the image the user has assigned as their profile pic
+    publicImages: string; // reference to the collection of the users images they have chosen to make viewable to anyone
+    privateImages: string; // reference to the collection of the users images they wish to be invisible to other users
+    friendsList: string[]; // list of references to user id's
  }
